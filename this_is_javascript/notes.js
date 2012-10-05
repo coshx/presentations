7dab67e02f4d221313d9baef4b829b9f97e678a3
Ben Taitelbaum <ben@coshx.com>
2012-10-05 04:49:05 -0400

    playing around before the talk

--- this_is_javascript/notes.js
+++ this_is_javascript/notes.js
@@ -1,94 +1,42 @@
-var objectLiteral = {
-  functionLiteral: function(arg1) {
-    console.log(this);
-    console.log(arg1);
-    console.log(arguments);
-    console.log(this.prototype);
-  }
-}
-
-var FunctionLiteral = function() {
-  console.log("constructor run.")
-}
-
-FunctionLiteral.prototype.addedMethod = function() { 
-  console.log("called method added to constructor"); 
-}
-myFunctionLiteral = new FunctionLiteral();
-//myFunctionLiteral.__proto__.constructor == FunctionLiteral
-myFunctionLiteral.addedMethod();
-// >> called method added to constructor
-
-
-// Singleton Pattern
-var global = this;
-var count = 0;
-
-function Singleton(count) {
-  if (this == global) {
-    if (!Singleton.instance) {
-      Singleton.instance = new Singleton(++count);
-    }
-    return Singleton.instance
-  } else {
-    this.count = count;
-  }
-}
-
-function badDesign(n)
-{
-  if (n === 0)
-  {
-    return n;
-  }
-  else
-  {
-    return
-    {
-      msg: "FAIL!"
-    }
-  }
-}
-
-function wrapper(func) {
-  var context = {};
-
-  return {
-    setContext: function(ctx) {
-      context = ctx;
-    },
-    getContext: function() {
-      return context;
-    },
-    apply: function(args) {
-      return func(context, args);
-    }
-  };
-};
-var incr = wrapper(function(context, x) { return context + x + 1; });
-incr.setContext(0);
-incr.apply(5); // 6
-incr.setContext(1);
-incr.apply(5); // 7
-
-function withContext(obj) {
-  var context = obj;
-
-  return {
-    setContext: function(ctx) {
-      context = ctx;
-    },
-    getContext: function(ctx) {
-      return context;
-    },
-    // currently just for argumentless functions
-    apply: function(func) {
-      return func(context);
-    }
-  };
+// object literals
+var someGlobalObject = {
+  anArray: [1,2,3],
+  aNumber: 3,
+  aFunction: function(x) { x + 1 },
+  aNestedObject: { x: 1, y: 2}
 };
 
-var phone = withContext({number : '555-1212'});
-function dial(context) { console.log("Hello, this is " + context.number); }
-phone.apply(dial);
+var myObject = {
+  value: 0,
+  incr: function() {
+    this.value += 1;
+    return this.value;
+  }
+};
 
+// prototypal inheritance
+var ObjectCreator = {
+  dispatch: function(obj, name, args) {
+    if (obj[name]) {
+      return obj[name](args);
+    } else if (obj.parent) {
+      return ObjectCreator.dispatch(obj.parent, name, args);
+    } else {
+      return null;
+    }
+  },
+  create: function() {
+    var obj = {};
+    obj.parent = ObjectCreator;
+    obj.dispatch = function(name, args) { return ObjectCreator.dispatch(obj, name, args); };
+    return obj;
+  }
+};
+
+var myObj = ObjectCreator.create();
+var otherObj = ObjectCreator.create();
+otherObj.foo = function() { return "hello!"; };
+myObj.parent = otherObj;
+
+ObjectCreator.foo = function() { return "foo"; }
+console.log(myObj.dispatch("foo"));
